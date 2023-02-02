0dad51db81f3d7a82d39a73699b0825af76032b0
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-02 14:24:40 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,13 @@
+export const item = {
+    "id": "0a54ea8f-7319-4da3-92ce-0b686501a32c",
+    "background_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png",
+    "box_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/49.png",
+    "give_amount": 1953,
+    "about_refer": "Send reach determine radio mind idea. Compare throughout hotel identify free little if who. Ask control management produce create since. Issue top recently himself society.",
+    "facebook_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/301.png",
+    "twitter_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/348.png",
+    "whatsapp_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/167.png",
+    "menu_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/357.png",
+    "invite_link": "Herself practice according cultural. Final writer tax huge decade must.",
+    "credit_earned": "Family laugh music."
+}
